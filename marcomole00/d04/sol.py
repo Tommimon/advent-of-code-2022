670152cc823e5d48f8c84974948e1c0002f2a8e1
marcomole00 <marcomole00@github.com>
2022-12-05 09:49:20 +0100

    day5 marcomole00

--- marcomole00/d04/sol.py
+++ marcomole00/d04/sol.py
@@ -1,3 +1,4 @@
+import re
 f = open("marcomole00/d04/input","r").read().split("\n")
 
 set_a,set_b = set(),set()
@@ -6,17 +7,19 @@ fully_contained_sets =0
 number_of_overlaps =0
 for l in f:
     elf1,elf2 = l.split(",")
-    elf1lb, elf1ub = map( lambda s: int(s) ,elf1.split("-"))
-    set_a = {x for x in range(elf1lb,elf1ub+1)}
+   # elf1lb, elf1ub = map( lambda s: int(s) ,elf1.split("-"))
+    elf1lb, elf1ub,elf2lb, elf2ub = map(int, re.findall(r'\d+',l))
+
+    set_a = set(range(elf1lb,elf1ub+1))
 
 
-    elf2lb, elf2ub = map( lambda s: int(s) ,elf2.split("-"))
-    set_b = {x for x in range(elf2lb,elf2ub+1)}
+   # elf2lb, elf2ub = map( lambda s: int(s) ,elf2.split("-"))
+    set_b = set( range(elf2lb,elf2ub+1))
 
-    if len(set_a & set_b) > 0:
+    if any(set_a & set_b):
         number_of_overlaps+=1
 
-    if (set_a & set_b) == set_a or (set_a & set_b) == set_b:
+    if set_a <= set_b or set_b<=set_a:
         fully_contained_sets +=1
         
 
