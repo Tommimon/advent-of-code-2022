b35a50416e583661ff380b49d791d02565a9ebd0
Matteo-Negro <62394493+Matteo-Negro@users.noreply.github.com>
2022-12-06 06:26:28 +0100

    Day04 real solution XD

--- MatteoBlack/d04/day04.py
+++ MatteoBlack/d04/day04.py
@@ -1,62 +1,32 @@
-import re
-
-
-def execute1(state: list, input: list) -> str:
-    result = ''
+def execute1(input: list) -> int:
+    result = 0
     for el in input:
-        qt, fr, to = (re.sub('move\\s|from\\s|to\\s', '', el)).split(' ')
-
-        for _ in range(int(qt)):
-            if len(state[int(fr) - 1]) > 0:
-                tmp = state[int(fr) - 1].pop(0)
-                state[int(to) - 1].insert(0, tmp)
-    for crate in state:
-        if len(crate) > 0:
-            result += crate.pop(0)
+        first, second = el.split(',')
+        first_st, first_en = first.split('-')
+        second_st, second_en = second.split('-')
+        if (int(first_st) >= int(second_st) and int(first_en) <= int(second_en)) or (
+                int(first_st) <= int(second_st) and int(first_en) >= int(second_en)):
+            result += 1
     return result
 
 
-def execute2(state_2, input: list) -> str:
-    result = ''
+def execute2(input: list) -> int:
+    result = 0
     for el in input:
-        qt, fr, to = (re.sub('move\\s|from\\s|to\\s', '', el)).split(' ')
-        tmp = []
-        for _ in range(int(qt)):
-            if len(state_2[int(fr) - 1]) > 0:
-                tmp.append(state_2[int(fr) - 1].pop(0))
-        for i in range(len(tmp)):
-            state_2[int(to) - 1].insert(i, tmp[i])
-    for crate in state_2:
-        if len(crate) > 0:
-            result += crate.pop(0)
+        first, second = el.split(',')
+        first_st, first_en = first.split('-')
+        second_st, second_en = second.split('-')
+        if (int(first_en) <= int(second_st) and int(first_st) >= int(second_en)) or (
+                int(first_en) >= int(second_st) and int(first_st) <= int(second_en)):
+            result += 1
     return result
 
 
 with open('input.txt') as f:
     lines = f.readlines()
-    sta = []
-    moves = []
-    mov = False
+    cleanLines = []
     for line in lines:
-        if line.strip() == '':
-            mov = True
-        elif mov:
-            moves.append(line.strip())
-        else:
-            sta.append(line.strip())
+        cleanLines.append(line.strip())
 
-    tmp_1 = [['O', 'F', 'L', 'S', 'R'], ['T', 'P', 'G', 'Q', 'Z', 'N'], ['B', 'Q', 'M', 'S'],
-             ['Q', 'B', 'C', 'H', 'J', 'Z', 'G', 'T'],
-             ['S', 'F', 'N', 'B', 'M', 'H', 'P'], ['G', 'V', 'L', 'S', 'N', 'Q', 'C', 'P'],
-             ['F', 'C', 'W'],
-             ['M', 'P', 'V', 'W', 'Z', 'G', 'H', 'Q'], ['R', 'N', 'C', 'L', 'D', 'Z', 'G']]
-    tmp_4 = [['O', 'F', 'L', 'S', 'R'], ['T', 'P', 'G', 'Q', 'Z', 'N'], ['B', 'Q', 'M', 'S'],
-             ['Q', 'B', 'C', 'H', 'J', 'Z', 'G', 'T'],
-             ['S', 'F', 'N', 'B', 'M', 'H', 'P'], ['G', 'V', 'L', 'S', 'N', 'Q', 'C', 'P'],
-             ['F', 'C', 'W'],
-             ['M', 'P', 'V', 'W', 'Z', 'G', 'H', 'Q'], ['R', 'N', 'C', 'L', 'D', 'Z', 'G']]
-    tmp_2 = [['N', 'Z'], ['D', 'C', 'M'], ['P']]
-    tmp_3 = [['N', 'Z'], ['D', 'C', 'M'], ['P']]
-
-print(f'DAY05_1 RESULT: {execute1(tmp_1, moves)}')
-print(f'DAY05_2 RESULT: {execute2(tmp_4, moves)}')
+print(f'DAY04_1 RESULT: {execute1(cleanLines)}')
+print(f'DAY04_2 RESULT: {execute2(cleanLines)}')
