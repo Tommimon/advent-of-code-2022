0c7a8741daf9d6064b42b3b2ad5f77ed0b4f89c5
marcomole00 <marcomole00@github.com>
2022-12-07 12:42:08 +0100

    day07 marcomole

---
+++ marcomole00/d07/sol.py
@@ -0,0 +1,47 @@
+from itertools import accumulate
+
+f = open("marcomole00/d07/input").read().split("\n")
+
+directories = {'//':0}
+
+current_directory = []
+
+for l in f:
+    match l.split():
+        case 'dir', dirname :
+            directories[''.join(current_directory)+dirname+"/"] = 0
+        case "$", 'ls':continue
+    
+        case '$', 'cd','..': 
+            current_directory.pop()
+        case '$', 'cd' , x : 
+            current_directory.append( x + "/")
+        case size , file:
+            for i in accumulate(current_directory):
+                directories[i] += int(size)
+            
+
+
+p1=0
+for val in directories.values():
+    if val <= 100000: p1+=val
+
+
+print(f"part1 is {p1}")
+
+
+## part2
+
+## Find the smallest directory that,
+#  if deleted, would free up enough space on the filesystem to run the update
+
+total = 70000000
+update_size= 30000000
+
+print(total - int(directories["//"]))
+dir_size = total
+for val in directories.values():
+    if  total -int(directories["//"]) + val >= update_size: dir_size= min(dir_size,val)
+
+
+print(dir_size)
