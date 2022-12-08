12391ef016496ec0a4140192dd9563c3239cf9af
Tommimon <tommimon@gmail.com>
2022-12-09 00:23:20 +0100

    Adding template for future problems

---
+++ Tommimon/template/part1and2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "py++.hpp"
+
+using namespace std;
+
+int main() {
+    ifstream ifs("input");
+    string input((istreambuf_iterator<char>(ifs)), (istreambuf_iterator<char>()));
+
+    vector<string> lines(split(input, "\n"));
+    int score1 = 0;
+    int score2 = 0;
+
+    for (int i = 0; i < lines.size(); ++i) {
+        string l = lines[i];
+
+    }
+
+    cout << score1 << endl;
+    cout << score2 << endl;
+}
