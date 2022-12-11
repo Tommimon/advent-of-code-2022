0cb4d5b09c69fca01c4c6232edda265d42809861
marcomole00 <marcomole00@gmail.com>
2022-12-11 13:00:20 +0100

    test commit

--- marcomole00/d10/sol.py
+++ marcomole00/d10/sol.py
@@ -1,27 +1,22 @@
-input = iter(open("marcomole00/d10/input").read().split("\n"))
-
+input = iter(open("marcomole00/d10/input").read().split("\n")+["end"])
 image = [['.' for _ in range(0,40)] for _ in range(0,6)]
 x =1
 exec = []
 cycle =1
-
 p1 =0
-
 while True:
     print(x)
     if cycle in (20,60,100,140,180,220): p1 += cycle*x
-    if cycle == 240: break
     if (cycle % 40) -1 in (x-1,x,x+1): image[cycle // 40][(cycle % 40)-1] =  "#"
     if exec ==[]:   
         match next(input).split():
             case "addx",amount: exec.append(int(amount))
-            case "noop": pass
+            case ["noop"]: pass
+            case ["end"]: break
     else:
         x += exec.pop()
     
     cycle+=1
-
 print(p1)
-
 for l in image:
     print("".join(l))
