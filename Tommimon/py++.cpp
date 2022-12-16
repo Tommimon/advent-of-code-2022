74dcc8a5a4ed29b2c4f57c18ba4db386f510bfc9
Tommimon <tommimon@gmail.com>
2022-12-16 10:45:10 +0100

    Adding namespace to library

--- Tommimon/py++.cpp
+++ Tommimon/py++.cpp
@@ -1,42 +1,41 @@
 #include <iostream>
 #include <vector>
 
-std::vector<std::string> split(std::string const &str, std::string const &delim)
-{
-    std::vector<std::string> out;
-    std::string remaining(str);
-    size_t cut = 0;
-    while (!remaining.empty() && cut != std::string::npos)
-    {
-        cut = remaining.find(delim);
-        if (cut == std::string::npos)
-            out.push_back(remaining.substr(0, remaining.size()));
-        else {
-            out.push_back(remaining.substr(0, cut));
-            remaining = remaining.substr(cut + delim.size(), remaining.size());
+namespace py {
+    std::vector<std::string> split(std::string const &str, std::string const &delim) {
+        std::vector<std::string> out;
+        std::string remaining(str);
+        size_t cut = 0;
+        while (!remaining.empty() && cut != std::string::npos) {
+            cut = remaining.find(delim);
+            if (cut == std::string::npos)
+                out.push_back(remaining.substr(0, remaining.size()));
+            else {
+                out.push_back(remaining.substr(0, cut));
+                remaining = remaining.substr(cut + delim.size(), remaining.size());
+            }
         }
+        return out;
     }
-    return out;
-}
 
-std::string replace(std::string const &str, std::string const &charsIn, std::string const &charsOut) {
-    std::string out(str);
-    size_t pos = 0;
-    while (pos != std::string::npos)
-    {
-        pos = out.find(charsIn, pos);
-        if (pos != std::string::npos) {
-            out.erase(pos, charsIn.length());
-            out.insert(pos, charsOut);
-            pos += charsOut.length();
+    std::string replace(std::string const &str, std::string const &charsIn, std::string const &charsOut) {
+        std::string out(str);
+        size_t pos = 0;
+        while (pos != std::string::npos) {
+            pos = out.find(charsIn, pos);
+            if (pos != std::string::npos) {
+                out.erase(pos, charsIn.length());
+                out.insert(pos, charsOut);
+                pos += charsOut.length();
+            }
         }
+        return out;
     }
-    return out;
-}
 
-int sum (std::vector<int> const &vec) {
-    int tot = 0;
-    for (auto e: vec)
-        tot += e;
-    return tot;
+    int sum(std::vector<int> const &vec) {
+        int tot = 0;
+        for (auto e: vec)
+            tot += e;
+        return tot;
+    }
 }
--- Tommimon/py++.hpp
+++ Tommimon/py++.hpp
@@ -3,41 +3,43 @@
 
 #include <functional>
 
-std::vector<std::string> split(std::string const &str, std::string const &delim);
-std::string replace(std::string const &str, std::string const &charsIn, std::string const &charsOut);
-int sum (std::vector<int> const &vec);
+namespace py {
+    std::vector<std::string> split(std::string const &str, std::string const &delim);
 
-template<typename T1, typename T2>
-std::vector<T2> map (std::function<T2(const T1)> const &func, std::vector<T1> const &vec)
-{
-    std::vector<T2> out;
-    for (const auto& e: vec)
-        out.push_back(func(e));
-    return out;
-}
+    std::string replace(std::string const &str, std::string const &charsIn, std::string const &charsOut);
 
-template<typename T>
-T max (std::vector<T> const &vec, std::function<int(T)> const &key)
-{
-    T max_elem;
-    int max_key;
-    bool first = true;
-    for (const auto& e: vec) {
-        int e_key = key(e);
-        if (first || e_key > max_key) {
-            first = false;
-            max_key = e_key;
-            max_elem = e;
-        }
+    int sum(std::vector<int> const &vec);
+
+    template<typename T1, typename T2>
+    std::vector<T2> map(std::function<T2(const T1)> const &func, std::vector<T1> const &vec) {
+        std::vector<T2> out;
+        for (const auto &e: vec)
+            out.push_back(func(e));
+        return out;
     }
-    return max_elem;
-}
 
-template<typename T>
-void remove(std::vector<T> &vec, T const &elem) {
-    std::remove_if(vec.begin(), vec.end(), [&](const auto &item) {
-        return elem == item;
-    });
+    template<typename T>
+    T max(std::vector<T> const &vec, std::function<int(T)> const &key) {
+        T max_elem;
+        int max_key;
+        bool first = true;
+        for (const auto &e: vec) {
+            int e_key = key(e);
+            if (first || e_key > max_key) {
+                first = false;
+                max_key = e_key;
+                max_elem = e;
+            }
+        }
+        return max_elem;
+    }
+
+    template<typename T>
+    void remove(std::vector<T> &vec, T const &elem) {
+        std::remove_if(vec.begin(), vec.end(), [&](const auto &item) {
+            return elem == item;
+        });
+    }
 }
 
 #endif //ADVENT_OF_CODE_2022_PY_HPP
